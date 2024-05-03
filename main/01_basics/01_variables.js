baa8ec8170896af22e4c0eade5729074c44ed88d
sanjana029 <106665091+sanjana029@users.noreply.github.com>
2024-05-03 20:44:02 +0000

    var changes

---
+++ main/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 3135
+let accountEmail = "sanjanamanwatkar58@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+
+// accountId = 3135
+
+accountEmail = "sanjanamanwatkar29@gmail.com"
+accountPassword = "2445"
+accountCity = "Robertsganj"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
