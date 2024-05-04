25b6ab694dfd62a022e13245895b5f5cf6fa291a
sanjana029 <106665091+sanjana029@users.noreply.github.com>
2024-05-04 19:59:12 +0000

    dataTypes done

---
+++ main/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3     //code readability should be high
+)
+console.log("Sanjana")
+
+
+let name = "sanjana"
+let age = 18
+let isLoggedIn = null
+let state;
+
+// number => 2 to power 53.
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof undefined); // output undefined
+console.log(typeof null);  // output object
