f457811d14e5d7209c4c825687573e95709b3b1f
sanjana029 <106665091+sanjana029@users.noreply.github.com>
2024-05-04 21:14:16 +0000

    03_conversionOperations

---
+++ main/01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "18abhinav"
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // output NaN because input is not a proper num
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => flase
+// "sanjana" => true
+
+
+
