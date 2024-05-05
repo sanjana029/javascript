3941a8c376839cc256ea02f9010d3a8f6d98e9f4
sanjana029 <106665091+sanjana029@users.noreply.github.com>
2024-05-05 21:51:46 +0000

    operations

--- main/01_basics/03_conversionOperation.js
+++ main/01_basics/03_conversionOperation.js
@@ -7,12 +7,44 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof isLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false
 // "" => flase
 // "sanjana" => true
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//****************************** OPERATIONS ***************************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+// console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2) (2 to the power 2)*
+//console.log(2/2)
+//console.log(2%2)
+
+let str1 = "hello"
+let str2 = " Abhinav"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 
 
